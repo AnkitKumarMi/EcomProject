db13367b28d347c355dbf9b651400986367875e9
Ankit Kumar Mishra <122213779+AnkitKumarMi@users.noreply.github.com>
2023-06-12 11:50:13 +0530

    create model in python package ther we create models

--- orders.py
+++ orders.py
@@ -1,18 +1,24 @@
-
-from django.shortcuts import render, redirect
-from django.contrib.auth.hashers import check_password
-from WebApp.models.customer import Customer
-from django.views import View
-from WebApp.models.product import Products
-from WebApp.models.orders import Order
+from django.db import models
+from .product import Products
+from .customer import Customer
+import datetime
 
 
+class Order(models.Model):
+    product = models.ForeignKey(Products,
+                                on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,
+                                 on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.IntegerField()
+    address = models.CharField (max_length=50, default='', blank=True)
+    phone = models.CharField (max_length=50, default='', blank=True)
+    date = models.DateField (default=datetime.datetime.today)
+    status = models.BooleanField (default=False)
 
-class OrderView(View):
+    def placeOrder(self):
+        self.save()
 
-
-    def get(self , request ):
-        customer = request.session.get('customer')
-        orders = Order.get_orders_by_customer(customer)
-        print(orders)
-        return render(request , 'MyApp/orders.html'  , {'orders' : orders})
+    @staticmethod
+    def get_orders_by_customer(customer_id):
+        return Order.objects.filter(customer=customer_id).order_by('-date')
