83c2bc351935ddb4bbb27ced723b395b4f062c4d
Ankit Kumar Mishra <122213779+AnkitKumarMi@users.noreply.github.com>
2023-06-12 11:51:32 +0530

    templatetags create new directory inside webapp

--- cart.py
+++ cart.py
@@ -1,14 +1,36 @@
 
-from django.shortcuts import render , redirect
+from django import template
 
-from django.contrib.auth.hashers import  check_password
-from WebApp.models.customer import Customer
-from django.views import  View
-from WebApp.models.product import Products
+register = template.Library ()
 
-class Cart(View):
-    def get(self , request):
-        ids = list(request.session.get('cart').keys())
-        products = Products.get_products_by_id(ids)
-        print(products)
-        return render(request , 'MyApp/cart.html' , {'products' : products} )
+
+@register.filter (name='is_in_cart')
+def is_in_cart(product, cart):
+    keys = cart.keys ()
+    for id in keys:
+        if int (id) == product.id:
+            return True
+    return False;
+
+
+@register.filter (name='cart_quantity')
+def cart_quantity(product, cart):
+    keys = cart.keys ()
+    for id in keys:
+        if int (id) == product.id:
+            return cart.get (id)
+    return 0;
+
+
+@register.filter (name='price_total')
+def price_total(product, cart):
+    return product.price * cart_quantity (product, cart)
+
+
+@register.filter (name='total_cart_price')
+def total_cart_price(products, cart):
+    sum = 0;
+    for p in products:
+        sum += price_total (p, cart)
+
+    return sum
